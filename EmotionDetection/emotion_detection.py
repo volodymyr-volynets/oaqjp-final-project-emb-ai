c502f596a1fc4dcaa38f3e4f9f45957d4f1e24a3
Volodymyr Volynets <volodymyr.volynets@gmail.com>
2025-04-19 16:24:49 -0400

    Fixes

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -6,8 +6,18 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)
-    formatted_response = json.loads(response.text)
+    
+    if response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
 
+    formatted_response = json.loads(response.text)
     dominant_emotion = formatted_response['emotionPredictions'][0]['emotion']['anger']
     dominant_name = 'anger'
     if formatted_response['emotionPredictions'][0]['emotion']['disgust'] > dominant_emotion:
